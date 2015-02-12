43b08e6893533260155a3fe096b5ecd6baaa34e3
clatfd <clatfd@gmail.com>
2015-02-12 23:14:11 +0800

    add file for queryhist and qrcode

---
+++ queryhist.php
@@ -0,0 +1,19 @@
+﻿
+<?php require_once("sys_conf.inc");
+$link_id=mysql_connect($DBHOST,$DBUSER,$DBPWD);
+mysql_select_db($DBNAME);
+mysql_query("SET NAMES 'utf8'");
+$sql="select * from hist where eng= '".$_GET['eng']."'";
+$actlist= array();
+$sqlresult=mysql_query($sql);
+while ($rowi=mysql_fetch_array($sqlresult)){
+	if($rowi['action']=='down'||$rowi['action']=='up')	
+	array_push($actlist, $rowi);
+}
+if (sizeof($actlist))
+	echo json_encode(array('status' => 1,'histdata'=>$actlist ));
+else
+	echo json_encode(array('status' => 0));
+mysql_close($link_id);
+
+?>
